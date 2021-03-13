ed3c648044de704dfaf836417a276d404fbb042d
Mitchell Mosure <hctimit@gmail.com>
2021-03-12 22:53:09 -0800

    feat: meta variadic filter

--- include/mosure/meta.hpp
+++ include/mosure/meta.hpp
@@ -7,6 +7,43 @@
 
 namespace mosure::rest::meta {
 
+    template <typename Arg, typename Tuple>
+    struct tuple_concat;
+
+    template <typename Arg, typename... Args>
+    struct tuple_concat<Arg, std::tuple<Args...>> {
+        using value = std::tuple<Arg, Args...>;
+    };
+
+    template <typename Arg, typename... Args>
+    using tuple_concat_t = typename tuple_concat<Arg, Args...>::value;
+
+    template <
+        template <typename...> typename Condition,
+        typename... Args
+    >
+    struct filter;
+
+    template <
+        template <typename...> typename Condition
+    >
+    struct filter<Condition> {
+        using value = std::tuple<>;
+    };
+
+    template <
+        template <typename...> typename Condition,
+        typename Arg,
+        typename... Args
+    >
+    struct filter<Condition, Arg, Args...> {
+        using value = std::conditional_v<
+            Condition<Arg>,
+            tuple_concat_t<Arg, typename filter<Args...>::value>,
+            typename filter<Args...>::value
+        >;
+    }
+
     template <typename R, typename T, typename... Args>
     struct MemFn {
         constexpr MemFn(R (T::*)(Args...) pm) : function(std::mem_fn(pm)) { }
